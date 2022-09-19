c35d9943755376bef490efec0b58d68e4c500673
crsddev <againnft@gmail.com>
2022-09-19 19:06:41 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("example@ex.com", "Example Explamlovich Eksik");
+
+        for (String key : hashMap.keySet()) {
+            String value = hashMap.get(key);
+            System.out.println("key: " + key + "\nvalue: " + value);
+        }
+    }
+}
