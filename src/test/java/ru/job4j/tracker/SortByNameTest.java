8d5b0f0f5f30178d7e8ba2dbf08eff490ef8f138
crsd <againnft@gmail.com>
2024-05-30 21:44:11 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/SortByNameTest.java
+++ src/test/java/ru/job4j/tracker/SortByNameTest.java
@@ -14,17 +14,16 @@ public class SortByNameTest {
     public void whenItemSortedAscByName() {
         List<Item> items = Arrays.asList(
                 new Item(1, "test1"),
+                new Item(7, "test7"),
                 new Item(4, "test4"),
-                new Item(2, "test2"),
-                new Item(9, "test9"),
-                new Item(7, "test7")
+                new Item(3, "test3")
         );
         List<Item> expected = Arrays.asList(
                 new Item(1, "test1"),
-                new Item(2, "test2"),
+                new Item(3, "test3"),
                 new Item(4, "test4"),
-                new Item(7, "test7"),
-                new Item(9, "test9")
+                new Item(7, "test7")
+
         );
         Collections.sort(items, new ItemAscByName());
         assertThat(items.toString()).isEqualTo(expected.toString());
